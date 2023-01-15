0774f422d9caafaa3459f5fd68e6fd1720ef2ca3
Lucemans <luc@lucemans.nl>
2023-01-15 19:34:11 +0000

    Add Github

--- www/src/components/Header.tsx
+++ www/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import { FC } from 'react';
 import { GitHub } from 'react-feather';
 
+import { Link } from './atom/Link';
+
 export const Header: FC = () => {
     return (
         <div className="w-full relative max-w-[1376px] mx-auto mb-8">
-            <div className="w-full max-w-[1376px] py-4 px-8 flex fixed z-10 border-b h-16 backdrop-blur-xl">
+            <div className="w-full max-w-[1376px] py-4 px-8 flex fixed z-10 border-b h-16 backdrop-blur-xl justify-between">
                 <h1>DevRel Events</h1>
                 <div>
-                    <GitHub />
+                    <Link
+                        href="https://github.com/v3xlabs/devrel-events"
+                        target="_blank"
+                    >
+                        <GitHub />
+                    </Link>
                 </div>
             </div>
             <div className="w-full h-16" />
--- www/src/index.tsx
+++ www/src/index.tsx
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
+if (
+    document.location.hostname !== 'localhost' &&
+    document.location.protocol !== 'https:'
+) {
+    document.location.protocol = 'https:';
+}
+
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
     <React.StrictMode>
         <App />

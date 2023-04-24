5c75bb5d712ecc3eed73743459db453df0dc3eee
Lucemans <luc@lucemans.nl>
2023-04-24 21:51:14 +0000

    Dump

--- www/src/components/calendar/Week.tsx
+++ www/src/components/calendar/Week.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 
 import { DEvent } from '../../data';
+import { colorFormIndex } from '../../lib/color-from-index';
 import { isInWeek } from '../../lib/inWeek';
 import { notWith } from '../../lib/utils';
 import { Day } from './Day';
@@ -28,53 +29,76 @@ export const Week: FC<{
                     now={now}
                 />
             ))}
-            {events
-                .filter((event) => isInWeek(event, start_date, end_date))
-                .map((event) => {
-                    const end = notWith(
-                        new Date(event.end_date || event.start_date),
-                        (event_end_date) =>
-                            event_end_date.getMonth() > month
-                                ? daysInWeek
-                                : Math.min(
-                                      Math.max(
-                                          event_end_date.getDate() -
-                                              start_day +
-                                              1,
-                                          0
-                                      ),
-                                      7
-                                  )
-                    );
-                    const start = notWith(
-                        new Date(event.start_date),
-                        (event_start_date) =>
-                            event_start_date.getMonth() < month
-                                ? 0
-                                : Math.max(
-                                      event_start_date.getDate() - start_day,
-                                      0
-                                  )
-                    );
+            <div className="absolute left-0 right-0 w-full bottom-0">
+                <div className="relative">
+                    {events
+                        .filter((event) =>
+                            isInWeek(event, start_date, end_date)
+                        )
+                        .reverse()
+                        .map((event, index) => {
+                            const end = notWith(
+                                new Date(event.end_date || event.start_date),
+                                (event_end_date) =>
+                                    event_end_date.getMonth() > month
+                                        ? daysInWeek
+                                        : Math.min(
+                                            Math.max(
+                                                event_end_date.getDate() -
+                                                start_day +
+                                                1,
+                                                0
+                                            ),
+                                            7
+                                        )
+                            );
+                            const start = notWith(
+                                new Date(event.start_date),
+                                (event_start_date) =>
+                                    event_start_date.getMonth() < month
+                                        ? 0
+                                        : Math.max(
+                                            event_start_date.getDate() -
+                                            start_day,
+                                            0
+                                        )
+                            );
 
-                    return (
-                        <div
-                            key={`${year}-${month}-${start_day}-${event.name}`}
-                            className="absolute bg-red-200 h-2 bottom-0 z-10 overflow-hidden"
-                            style={{
-                                left: `${(start / 7) * 100}%`,
-                                right: `${((7 - end) / 7) * 100}%`,
-                            }}
-                        >
-                            <span
-                                className="text-[0.5rem] block"
-                                style={{ marginTop: '-0.25em' }}
-                            >
-                                {event.name}
-                            </span>
-                        </div>
-                    );
-                })}
+                            return (
+                                <div className="w-full flex">
+                                    <div
+                                        className="h-1"
+                                        style={{
+                                            width: `${(start / 7) * 100}%`,
+                                        }}
+                                    />
+                                    <div
+                                        key={`${year}-${month}-${start_day}-${event.name}`}
+                                        className="grow h-2 bottom-0 z-10 overflow-hidden"
+                                        style={{
+                                            background: colorFormIndex(index),
+                                            // left: `${(start / 7) * 100}%`,
+                                            // right: `${((7 - end) / 7) * 100}%`,
+                                        }}
+                                    >
+                                        <span
+                                            className="text-[0.5rem] block"
+                                            style={{ marginTop: '-0.25em' }}
+                                        >
+                                            {event.name}
+                                        </span>
+                                    </div>
+                                    <div
+                                        className="h-1"
+                                        style={{
+                                            width: `${((7 - end) / 7) * 100}%`,
+                                        }}
+                                    />
+                                </div>
+                            );
+                        })}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ www/src/lib/color-from-index.ts
@@ -0,0 +1,5 @@
+const colors = ['#123ffe', '#aa2a00', '#11ee00'];
+
+export const colorFormIndex = (index: number) => {
+    return colors[index] || '#111';
+};
